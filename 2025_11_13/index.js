3fbe12a0636f434adea8a4194e04e737920e5873
Kuba <kuba.owczarzak@wp.pl>
2025-11-26 09:13:41 +0100

    polaczenie routerow, middleware bledow w index.js

--- 2025_11_13/index.js
+++ 2025_11_13/index.js
@@ -1,10 +1,26 @@
-const express = require('express')
-
+require('dotenv').config();
+const express = require('express');
 const app = express()
 
+app.use(express.json());
 
 
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
-})
+const WpisyRouter = require('./routes/wpisy');
+const KomentarzeRouter = require('./routes/komentarze');
+const KategorieRouter = require('./routes/kategorie');
+
+app.use('/routes/wpisy', WpisyRouter);
+app.use('/routes/komentarze', KomentarzeRouter);
+app.use('/routes/kategorie', KategorieRouter);
+
+app.use((err, res, req, next) => {
+  console.log(err);
+  res.status(err.status || 500).json({error: err.message || 'Server error'})
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App listening on port: ${port}`));
 
